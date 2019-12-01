73e977362627b30e5c9c252b837946d7a1b7a0ed
Farnaz Kakhsaz <farnaz.kakhsaz@gmail.com>
2019-12-01 15:55:06 +0330

    initialize project

---
+++ Greetr.js
@@ -0,0 +1,136 @@
+(function (global, $){
+    
+    // "new" an object
+    let Greetr = function(firstName, lastName, language){
+        return new Greetr.init(firstName, lastName, language)
+    }
+    
+    // hidden within the scope of the IIFE and never directly accessible
+    let supportedLangs = ["en", "es"];
+    
+
+    // we have two type of greetings informal and formal
+    let greetings = {
+        en: "Hello",
+        es: "Hola",
+    }
+    let formalGreetings = {
+        en: "Greetingd",
+        es: "Saludos"
+    }
+    
+    // message that log in to the console
+    let logMessages = {
+        en: "Logged in",
+        es: 'Inició sesión'
+    }
+
+    // prototype holds methods (to save memory space)
+    Greetr.prototype = {
+        
+        // "this" refers to the callings object at execution time
+        fullName: function() {
+            return `${this.firstName} ${this.lastName}`
+        },
+        
+        validate: function() {
+            // check that is a valid language
+            // refernces the externally inaccessible with closure
+            if (supportedLangs.indexOf(this.language)  === -1) {
+                throw "Invalid language";   
+            }
+        }, 
+        
+        // retrieve messages from an object by referring to properties using [] syntax
+        greeting: function() {
+            return `${greetings[this.language]} ${this.firstName}!`
+        },
+        
+        formalGreeting: function() {
+            return `${greetings[this.language]}, ${this.fullName()}`
+        },
+        
+        // chainable methods return their own containing object
+        greet: function(formal) {
+            let msg;
+            
+            // if undefined or null it will be coerced to 'false'
+            if (formal) {
+                msg = this.formalGreeting();  
+            }
+            else {
+                msg = this.greeting();  
+            }
+
+            if (console) {
+                console.log(msg);
+            }
+
+            // 'this' refers to the calling object at execution time
+            // makes the method chainable
+            return this;
+        },
+        
+        log: function() {
+            if(console) {
+                console.log(`${logMessages[this.language]}: ${this.fullName()}`)
+            }
+            // make chainable
+            return this
+        },
+        
+        setLang: function(lang) {
+
+            // set the language
+            this.language = lang;
+            
+            // check that the language we use is a valid or not
+            this.validate();
+
+            // make chainable
+            return this;
+        },
+
+        HTMLGreeting: function(selector, formal) {
+            if(!$) {
+                throw "jQuery not loaded"
+            }
+
+            if(!selector) {
+                throw "Missing jQuery selector"
+            }
+
+            // determine the message
+            var msg;
+            if(formal) {
+                msg = this.formalGreeting()
+            } else {
+                msg= this.greeting()
+            }
+
+            // inject the message in the chosen place in the DOM
+            $(selector).html(msg);
+
+            // make chainable
+            return this
+        }
+    }
+    
+    // the actual object is created here, allowing us to 'new' an object without calling 'new'
+    Greetr.init = function(firstName, lastName, language) {
+        
+        this.firstName = firstName || "";
+        this.lastName = lastName || "";
+        this.language = language || "en"
+        
+        this.validate();
+    }
+    
+    // trick borrowed from jQuery so we don't have to use the 'new' keyword
+    Greetr.init.prototype = Greetr.prototype;
+
+    // attach our Greetr to the global object, and provide a shorthand '$G' for ease our poor fingers
+    global.Greetr = global.G$ = Greetr;
+    
+    
+})(window, jQuery)
